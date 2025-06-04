963f8238f21a6aa7621a3aa2eaab8ca1e9bfb1f6
ViniciusGr63 <128187154+ViniciusGr63@users.noreply.github.com>
2025-06-04 21:29:17 +0000

    correção futures

--- api/utils/image_preprocessing.py
+++ api/utils/image_preprocessing.py
@@ -4,16 +4,18 @@ import io
 
 def preprocess_image(image_bytes):
     image = Image.open(io.BytesIO(image_bytes)).convert('L')
-    binary = np.array(image) < 128  # Binariza, assumindo forma escura no fundo claro
+    binary = np.array(image) < 128  # Binariza a imagem
 
     coords = np.argwhere(binary)
     if coords.size == 0:
-        # Se não tem forma detectada, retorna zeros
-        return np.array([[0, 0]])
+        return np.array([[0, 0, 0]])  # Retorna 3 características (largura, altura, 0)
+
     y_min, x_min = coords.min(axis=0)
     y_max, x_max = coords.max(axis=0)
     width = x_max - x_min
     height = y_max - y_min
 
-    features = np.array([width, height]).reshape(1, -1)
+    # Adicionando uma característica extra (por exemplo, área ou perímetro)
+    area = width * height  # Exemplificando com a área
+    features = np.array([width, height, area]).reshape(1, -1)  # Agora 3 características
     return features
