e1714ba248374f2ed023e22bad56db272cc1a65a
ViniciusGr63 <128187154+ViniciusGr63@users.noreply.github.com>
2025-06-05 19:27:46 +0000

    correção predict2

--- api/models/captcha_model.py
+++ api/models/captcha_model.py
@@ -18,19 +18,14 @@ class MLModels:
         # self.x.load()
 
     def predict_shape(self, features):
-        # Exemplo simples: roda todos e retorna qual modelo tem maior confiança
-        # ou simplesmente roda cada um como classificador binário
-        # Aqui vamos supor que cada modelo prevê 1 (não) ou 0 (sim)
         results = {
-            'triangle': self.triangle.predict(features)[1],
-            # 'square': self.square.predict(features)[1],
-            # 'circle': self.circle.predict(features)[1],
-            # 'x': self.x.predict(features)[1]
+            'triangle': self.triangle.predict(features)[0],  # pegar o primeiro e único valor
+            # 'square': self.square.predict(features)[0],
+            # 'circle': self.circle.predict(features)[0],
+            # 'x': self.x.predict(features)[0]
         }
         
-        # Seleciona o modelo que retornou 1, ou nenhum
         for shape, pred in results.items():
-            if pred == 0:
+            if pred == 1:  
                 return shape
         return "unknown"
-        
