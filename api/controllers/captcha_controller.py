8aca45126f1de34e0844c7ec1dec236870bad155
ViniciusGr63 <128187154+ViniciusGr63@users.noreply.github.com>
2025-06-06 23:25:32 +0000

    v1

--- api/controllers/captcha_controller.py
+++ api/controllers/captcha_controller.py
@@ -1,23 +1,46 @@
+# from flask import request, jsonify
+# from ..models.captcha_model import MLModels
+# from ..utils.image_preprocessing import preprocess_image
+
+# ml_models = MLModels()
+
+# def recognize():
+  
+#     if 'image' not in request.files:
+#       return jsonify({'error': 'No image provided'}), 400
+
+#     if 'shape' not in request.form:
+#       return jsonify({'error': 'No shape provided'}), 400
+
+    
+#     image_file = request.files['image']
+#     image_bytes = image_file.read()
+
+#     shape_exp = request.form.get('shape')
+    
+    
+#     features = preprocess_image(image_bytes) 
+#     shape = ml_models.predict_shape(features,shape_exp)
+    
+#     return jsonify({'result': bool(shape)})
+
 from flask import request, jsonify
 from ..models.captcha_model import MLModels
+
 from ..utils.image_preprocessing import preprocess_image
 
 ml_models = MLModels()
 
 def recognize():
+
     if 'image' not in request.files:
         return jsonify({'error': 'No image provided'}), 400
-    if 'shape' not in request.files:
-        return jsonify({'error': 'No image provided'}), 400
-    
+
     image_file = request.files['image']
     image_bytes = image_file.read()
-
-    shape_exp = request.form.get('shape')
     
+    features = preprocess_image(image_bytes)  
     
-    features = preprocess_image(image_bytes) 
-    shape = ml_models.predict_shape(features,shape_exp)
+    shape = ml_models.predict_shape(features)
     
-    return jsonify({'result': bool(shape)})
-
+    return jsonify({'shape': shape})
--- api/models/captcha_model.py
+++ api/models/captcha_model.py
@@ -1,13 +1,22 @@
 
 from ..modelsML.shapes import Shapes
 
+# class MLModels:
+#     def __init__(self):
+#         self.shape = Shapes()
+#         self.shape.load_model()
+
+#     def predict_shape(self, features, shape_exp):
+#         response = self.shape.predict(features)  # deve retornar uma string, ex: "circle"
+
+#         # Garante que ambos estão em lowercase e sem espaços extras
+#         return response.strip().lower() == shape_exp.strip().lower()
+
 class MLModels:
     def __init__(self):
         self.shape = Shapes()
         self.shape.load_model()
 
-    def predict_shape(self, features, shape_exp):
-        response = self.shape.predict(features)  # deve retornar uma string, ex: "circle"
 
-        # Garante que ambos estão em lowercase e sem espaços extras
-        return response.strip().lower() == shape_exp.strip().lower()
+    def predict_shape(self, features):
+        return self.shape.predict(features)
